fec60a8093f8f32d9ed9492f17e94fc3ea8b2211
capital terefe <capital.terefe@gmail.com>
2018-08-21 06:59:33 -0400

    configuration updated

--- basics/conftest.py
+++ basics/conftest.py
@@ -1,20 +1,24 @@
 import pytest
 
+
 @pytest.fixture(scope="class")
-def oneTimeSetUp(request,browser):
+def oneTimeSetUp(request, browser):
     print('Running one time setup')
-    if browser== 'chrome':
+    if browser == 'chrome':
         print('running on chrome')
-        browserType='chrome'
+        browserType = 'chrome'
     else:
         print('running on other browser')
     if request.cls is not None:
-        #to make the browser type available accross our module
-        request.cls.browserType=browserType
+        # to make the browser type available accross our module
+        request.cls.browserType = browserType
     yield browserType
     print('Running on time tearDown')
+
+
 def pytest_addoption(parser):
     parser.addoption("--browser")
+
 
 @pytest.fixture(scope='session')
 def browser(request):
--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -1,22 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
+import sys
+import unittest
 from pages.home.login_page import LoginPage
-class LogingTests():
+
+
+class LogingTests(unittest.TestCase):
     def test_validLogin(self):
-        baseUrl="https://letskodeit.teachable.com/"
-        driver=webdriver.Firefox()
+        print(sys.path)
+        print("webdriver module location"+webdriver.__file__)
+        chromedriver = "/Users/ct/Documents/devTools/chromedriver"
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        driver = webdriver.Chrome(chromedriver)
+        baseUrl = "https://letskodeit.teachable.com/"
         driver.maximize_window()
         driver.implicitly_wait(3)
         driver.get(baseUrl)
-        lp=LoginPage(driver)
-        lp.login("test@email.com","abcabc")
-        
-        
-        userIcon=driver.find_element(By.XPATH,".//*[@id='navbar']")
+        lp = LoginPage(driver)
+        lp.login("test@email.com", "abcabc")
+
+        userIcon = driver.find_element(By.XPATH, ".//*[@id='navbar']")
         if userIcon is not None:
             print("login successful")
         else:
             print('login failed')
-
-
-
